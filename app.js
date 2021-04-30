13c466bba346c3860d2b739f77b7696d3661716e
Dikshant Rajwal <drajwal1511@gmail.com>
2021-04-30 21:09:17 +0530

    port to 2000

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ const sequelize = require("./database");
 })();
 const miscRoutes = require("./routes/misc");
 app.use(miscRoutes);
-app.listen(3000,()=>{
+app.listen(2000,()=>{
     console.log("server up at port 80");
 })
