8eb5a12b7f356dbeb9233c48d91c11ddca220b3e
Dikshant Rajwal <drajwal1511@gmail.com>
2021-04-30 18:01:23 +0530

    fixed IP bug

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ const sequelize = require("./database");
 })();
 const miscRoutes = require("./routes/misc");
 app.use(miscRoutes);
-app.listen(3000,process.env.IP,()=>{
-    console.log("server up at port 3000");
+app.listen(80,()=>{
+    console.log("server up at port 80");
 })
